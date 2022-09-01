502d3fd98ed6c2ed813175a545f48f57aaa71a7e
Flu3nt <gnedyalkov94@gmail.com>
2022-09-01 10:53:01 +0300

    Set up a basic express server

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require("express")
+const app = express()
+
+const port = 1500
+
+// routes
+// get request
+app.get("/hello", (req, res) => {
+  res.send(`Hello World`)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}...`)
+})

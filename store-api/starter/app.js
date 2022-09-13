0a781d5d68593b1539978594a04c6420d78c6caf
Flu3nt <gnedyalkov94@gmail.com>
2022-09-13 17:10:48 +0300

    Create a basic server for Store API

--- store-api/starter/app.js
+++ store-api/starter/app.js
@@ -1,1 +1,33 @@
-console.log("Store API")
+require("dotenv").config()
+// async errors
+
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3001
+
+const notFoundMiddleware = require("./middleware/not-found")
+const errorMiddleware = require("./middleware/error-handler")
+
+// express json middleware
+app.use(express.json())
+
+// route
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>products route<a/>")
+})
+
+// products route
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+// start function
+const start = async () => {
+  try {
+    // connect db
+    app.listen(port, console.log(`Listening on port: ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()

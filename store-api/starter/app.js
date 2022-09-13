33a65a92b3dbb26954bd954152a2d894678a382c
Flu3nt <gnedyalkov94@gmail.com>
2022-09-13 16:12:39 +0300

    Refactored folder

---
+++ store-api/starter/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+
+// async errors
+
+const express = require("express")
+const app = express()
+
+const notFoundMiddleware = require("./middleware/not-found")
+const errorMiddleware = require("./middleware/error-handler")
+
+// middleware
+app.use(express.json())
+
+// routes
+
+app.get("./", (req, res) => {
+  res.send("<h1>Store API </h1>")
+})

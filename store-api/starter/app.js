57f84029ccc5d319f098a7addf54e33305b86ad9
Flu3nt <gnedyalkov94@gmail.com>
2022-09-13 16:15:33 +0300

    Clean up

--- store-api/starter/app.js
+++ store-api/starter/app.js
@@ -1,18 +1,1 @@
-require("dotenv").config()
-
-// async errors
-
-const express = require("express")
-const app = express()
-
-const notFoundMiddleware = require("./middleware/not-found")
-const errorMiddleware = require("./middleware/error-handler")
-
-// middleware
-app.use(express.json())
-
-// routes
-
-app.get("./", (req, res) => {
-  res.send("<h1>Store API </h1>")
-})
+console.log("Store API")

b56dd0308012b505c5226d0ec243ce37d517e0be
Flu3nt <gnedyalkov94@gmail.com>
2022-09-15 14:44:26 +0300

    Implement pagination

--- store-api/starter/controllers/products.js
+++ store-api/starter/controllers/products.js
@@ -1,7 +1,9 @@
 const Product = require("../models/product")
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select("name price")
+  const products = await Product.find({}).sort("name").select("name price")
+  // .limit(10)
+  // .skip(1)
   res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -21,8 +23,6 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" }
   }
 
-  // console.log(queryObject)
-
   let result = Product.find(queryObject)
 
   // sort
@@ -37,6 +37,13 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ")
     result = result.select(fieldsList)
   }
+
+  // pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
 
   const products = await result
 
